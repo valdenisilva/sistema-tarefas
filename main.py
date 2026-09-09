4d121c74d86fdff1cd08a4bc995db554465b0edd
Bruno <brunogoulartdoss@gmail.com>
2026-09-09 14:31:26 -0300

    ajuste no no codigo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from cadastro_tarefa import criar_cadastro_tarefa
 
+from menu_tarefas import Cadastrar_tarefa,Listar_tarefas,Atualizar_situação_tarefa
 
 print("Sistema Web de Gestão de Tarefas")
--- menu_tarefas.py
+++ menu_tarefas.py
@@ -13,35 +13,34 @@ def Listar_tarefas(tarefas):
     if len(tarefas) == 0:
         print("nenhuma tarefa cadastrada!")
     else:
-        print("/n tarefa cadastrada com sucesso")
+        print("\n tarefa cadastrada com sucesso")
         for i in range(len(tarefas)):
-            print(i, " ", tarefas[i])
+            print(i+1, " ", tarefas[i])
 
 def Atualizar_situação_tarefa(tarefas):
-    indce = int(input("digite o indce que deseja atualizar "))
-    indce = -1 
-    if indce >= 0 and indce < len(tarefas) -1:
+    indice = int(input("digite o indce que deseja atualizar ")) -1 
+    if indice >= 0 and indice < len(tarefas):
         nova_tarefa = input("digite a nova tarefa ")
-        tarefas[indce] = nova_tarefa
-        print("tarefa concruida")
+        tarefas[indice] = nova_tarefa
+        print("tarefa concluida")
     else:
-        print("tarefa inesistente")
+        print("tarefa inexistente")
         
 while True:
     print("tarefas")
-    print("1,cadastrar")
-    print("2,listar")
-    print("3, atualizar")
-    print("4, sair")
+    print("1:cadastrar")
+    print("2:listar")
+    print("3: atualizar")
+    print("4: sair")
     opcao = input("Escolha uma opcao ")
 
     if opcao == '1':
         Cadastrar_tarefa(tarefas)
-    if opcao =='2':
+    elif opcao =='2':
         Listar_tarefas(tarefas)
-    if opcao == '3':
+    elif opcao == '3':
         Atualizar_situação_tarefa(tarefas)
-    if opcao == '4':
+    elif opcao == '4':
         print('Encerrar_sistema...')
         break
     else:
