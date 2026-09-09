a230b8f52a83aacc181eb188e803cfb8759c0202
Bruno <brunogoulartdoss@gmail.com>
2026-09-09 12:20:57 -0300

    adiconando uma função no cadastro de tarefas

--- cadastro_tarefa.py
+++ cadastro_tarefa.py
@@ -1,23 +1,27 @@
-print(" Cadastro de Tarefa ")
+def criar_cadastro_tarefa():
 
-titulo = input("Título da tarefa: ")
+    print(" Cadastro de Tarefa ")
 
-prioridade = int(input("Prioridade (1 a 5): "))
-prazo_horas = float(input("Prazo estimado em horas: "))
+    titulo = input("Digite o título da tarefa: ")
 
-resposta_urgencia = input("A tarefa é urgente? (sim/nao): ")
-urgente = resposta_urgencia == "sim"
+    prioridade = int(input("Prioridade (1 a 5): "))
+    prazo_horas = float(input("Prazo estimado em horas: "))
 
-fator_esforco = 1.2
-esforco_estimado = prazo_horas * fator_esforco
-prioridade_alta = prioridade >= 4
-prioritaria = prioridade_alta or urgente
+    resposta_urgencia = input("A tarefa é urgente? (sim/nao): ").
+    urgente = resposta_urgencia == "sim"
 
-print("\n=== Resumo da Tarefa ===")
-print(f"Título: {titulo}")
-print(f"Prioridade: {prioridade}")
-print(f"Prazo informado: {prazo_horas:.2f} horas")
-print(f"Esforço estimado: {esforco_estimado:.2f} horas")
-print(f"Urgente: {urgente}")
-print(f"Prioridade alta: {prioridade_alta}")
-print(f"Deve ser tratada como prioritária: {prioritaria}")
+    fator_esforco = 1.2
+    esforco_estimado = prazo_horas * fator_esforco
+    prioridade_alta = prioridade >= 4
+    prioritaria = prioridade_alta or urgente
+
+    print("\n=== Resumo da Tarefa ===")
+    print(f"Título: {titulo}")
+    print(f"Prioridade: {prioridade}")
+    print(f"Prazo informado: {prazo_horas:.2f} horas")
+    print(f"Esforço estimado: {esforco_estimado:.2f} horas")
+    print(f"Urgente: {urgente}")
+    print(f"Prioridade alta: {prioridade_alta}")
+    print(f"Deve ser tratada como prioritária: {prioritaria}")
+
+criar_cadastro_tarefa()
